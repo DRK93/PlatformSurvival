4d78fc14d15e1f34399fddf44348232bb28ffd74
DRK93 <86095568+DRK93@users.noreply.github.com>
2023-11-02 18:46:19 +0100

    Add Main Menu
    Added Main Menu and small changes in GameMainManager because of that.

--- Assets/Scripts/GameMainManager.cs
+++ Assets/Scripts/GameMainManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,10 +9,52 @@ public class GameMainManager : MonoBehaviour
 {
     public GameObject startScreen;
     public UnityEvent OnGameStart;
-
     private SpawnManager m_SpawnManager;
+
+    private bool m_gameStarted;
     // Start is called before the first frame update
     void Start()
+    {
+        ShowMenu();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            ShowMenu();
+        }
+    }
+
+    private void ShowMenu()
+    {
+        Time.timeScale = 0f;
+        startScreen.SetActive(true);
+    }
+
+    private void HideMenu()
+    {
+        startScreen.SetActive(false);
+    }
+
+    public void ResumeGame()
+    {
+        if (m_gameStarted)
+        {
+            Time.timeScale = 1f;
+            HideMenu();
+        }
+    }
+
+    public void PlayGame()
+    {
+        if (!m_gameStarted)
+        {
+            Time.timeScale = 1f;
+            StartGameRun();
+        }
+    }
+    private void StartGameRun()
     {
         m_SpawnManager = FindObjectOfType<SpawnManager>();
         var elevators = FindObjectsOfType<Elevator>();
@@ -19,8 +62,10 @@ public class GameMainManager : MonoBehaviour
         {
             OnGameStart.AddListener(elevators[i].OnGameStart);
         }
-    }
 
+        m_gameStarted = true;
+        StartGame();
+    }
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
@@ -31,5 +76,10 @@ public class GameMainManager : MonoBehaviour
         OnGameStart.Invoke();
         m_SpawnManager.StartSpawning();
         startScreen.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
